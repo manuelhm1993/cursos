685d807489db828e42ba3951dff3438607ce0ca3
Manuel Henriquez <manuelhm1993@gmail.com>
2026-07-08 16:21:32 -0400

    Introducción, configuración, operaciones básicas CRUD, db creada, tablas usuarios y turnos_medicos relacionadas y gráfico en .xlsx como recurso visual

---
+++ curso-sql/queries/operacones crud/insert.sql
@@ -0,0 +1,8 @@
+insert into usuarios (nombre, apellido, edad) 
+values 
+	('Manuel', 'Henriquez', 32),
+	('Sugey', 'Godoy', 22),
+	('Carlos', 'Vera', 36),
+	('Ender', 'Paredes', 30),
+	('Jean', 'Villegas', 35),
+	('Fernando', 'Barrera', 35);
---
+++ curso-sql/queries/operacones crud/insert_relacion_tablas.sql
@@ -0,0 +1,3 @@
+insert into turnos_medicos (profesional, usuario_id, motivo, horario)
+values
+("Dr. Ramirez", 2, "Síntomas de embarazo", "07/07/2026 - 3:50pm");
