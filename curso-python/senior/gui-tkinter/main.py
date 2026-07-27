e9c362c705ad8902850c8bdd96e60e5555a3e708
Manuel Henriquez <manuelhm1993@gmail.com>
2026-07-27 18:32:53 -0400

    feature: filedialog, se aprendió el uso de las ventanas que se delegan al sistema operativo para guardar archivos, abrirlos y manipularlos, se usan las rutas y se trabaja a partir de allí

--- curso-python/senior/gui-tkinter/main.py
+++ curso-python/senior/gui-tkinter/main.py
@@ -7,7 +7,7 @@ import tkinter as tk
 # from widgets.button.button_view import ButtonView
 # from widgets.radio_button.radio_button_view import RadioButtonView
 # from widgets.check_button.check_button_view import CheckButtonView
-from widgets.menu_messagebox.menu_messagebox_view import MenuMessageBox
+from widgets.tk_filedialog.tk_filedialog_view import FileDialogView
 from ui.window import MainWindow
 
 # Punto de entrada del programa
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     root = tk.Tk()
 
     # Instanciamos la vista concreta
-    main_view = MenuMessageBox(500, 350)
+    main_view = FileDialogView(500, 350)
 
     # Inyectamos la raíz en nuestra clase
-    app = MainWindow(root, "GUI TKinter - Uso Menu & Messagebox", main_view)
+    app = MainWindow(root, "GUI TKinter - Uso messagebox & filedialog", main_view)
     app.redimensionar(True)
 
     # Arrancamos el mainloop (bucle infinito que escucha eventos)
