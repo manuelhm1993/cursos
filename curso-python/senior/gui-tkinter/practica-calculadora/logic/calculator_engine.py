e42d9ac4fe4a46c78c657566021bc144a13cbe58
Manuel Henriquez <manuelhm1993@gmail.com>
2026-07-27 00:05:00 -0400

    refactor: se corrigió la lógica de documentación dentro de la carpeta docs para seguir el estándar en todos los proyectos

--- curso-python/senior/gui-tkinter/practica-calculadora/logic/calculator_engine.py
+++ curso-python/senior/gui-tkinter/practica-calculadora/logic/calculator_engine.py
@@ -2,17 +2,17 @@ from functools import reduce
 
 class CalculatorEngine:
     @staticmethod
-    def sumar(*numeros: int) -> int:
+    def sumar(*numeros: str) -> int:
         return sum(map(lambda n: int(n), numeros))
     
     @staticmethod
-    def restar(*numeros: int) -> int:
+    def restar(*numeros: str) -> int:
         return reduce(lambda acumulado, numero: int(acumulado) - int(numero), numeros)
 
     @staticmethod
-    def multiplicar(*numeros: int) -> int:
+    def multiplicar(*numeros: str) -> int:
         return reduce(lambda acumulado, numero: int(acumulado) * int(numero), numeros)
 
     @staticmethod
-    def dividir(*numeros: float) -> float:
+    def dividir(*numeros: str) -> float:
         return reduce(lambda acumulado, numero: float(acumulado) / float(numero), numeros)
