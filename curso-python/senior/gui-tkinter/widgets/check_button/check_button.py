7c5b722d3b67f304cb0bb6fe92e51211460769e8
Manuel Henriquez <manuelhm1993@gmail.com>
2026-07-27 04:45:01 -0400

    docs: Teoría del garbage collector en md

--- curso-python/senior/gui-tkinter/widgets/check_button/check_button.py
+++ curso-python/senior/gui-tkinter/widgets/check_button/check_button.py
@@ -9,7 +9,7 @@ class CheckButtonView(ViewInterface):
         self._alto  = alto
 
         # El garbadge collector elimina las variables de la memoria al terminar los métodos
-        self._logo     = tk.PhotoImage(file=ASSETS_PATH / "widgets/logo-avion.gif")
+        self._logo     = tk.PhotoImage(file=ASSETS_PATH / "widgets/logo-avion.png")
         self._destinos = {
             "Playa": tk.IntVar(), 
             "Montaña": tk.IntVar(), 
@@ -29,15 +29,21 @@ class CheckButtonView(ViewInterface):
         return frame
 
     def _construir_widgets(self, frame: tk.Frame) -> None:
-        tk.Label(frame, image=self._logo).pack(pady=(25, 10))
-        tk.Label(frame, text="Elige un destino").pack(pady=(0, 10))
+        frame_logo = tk.Frame(frame)
+        frame_logo.pack(expand=True, fill="both")
+
+        frame_botones = tk.Frame(frame)
+        frame_botones.pack(expand=True, fill="both")
+
+        tk.Label(frame_logo, image=self._logo).pack(pady=(25, 10))
+        tk.Label(frame_botones, text="Elige un destino").pack(pady=(0, 10))
 
         for nombre_destino, variable_control in self._destinos.items():
-            # El botón asigna 0-1 dependiendo del estado
-            tk.Checkbutton(frame, text=nombre_destino, variable=variable_control, command=self._imprimir_destinos).pack()
+            # El botón asigna 0-1 dependiendo del estado. Pero si se quiere especificar explícitamente se usan onvalue y offvalue
+            tk.Checkbutton(frame_botones, text=nombre_destino, variable=variable_control, onvalue=1, offvalue=0, command=self._imprimir_destinos).pack()
 
-        self._confirmacion_viaje = tk.Label(frame, text="")
-        self._confirmacion_viaje.pack(pady=20)
+        self._confirmacion_viaje = tk.Label(frame_botones, text="")
+        self._confirmacion_viaje.pack(pady=(20, 0))
 
     def _imprimir_destinos(self):
         # 1. Filtramos directamente los destinos que tengan valor 1 usando List Comprehension
