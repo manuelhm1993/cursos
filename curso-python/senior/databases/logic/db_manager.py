ee72e4d0a8b48076ab65b3aac9f0b9524f77c55c
Manuel Henriquez <manuelhm1993@gmail.com>
2026-07-27 21:28:00 -0400

    feature: Operaciones básicas CRUD cubieras, solo falta el select y el insert múltiple

--- curso-python/senior/databases/logic/db_manager.py
+++ curso-python/senior/databases/logic/db_manager.py
@@ -12,7 +12,13 @@ class DBManager:
         precio DOUBLE(16,2)
     )
     """
+    _query_inster = """
+    INSERT INTO productos (nombre_articulo, seccion, precio) 
+    VALUES
+    ("BALÓN", "DEPORTES", 15)
+    """
 
+    # ----------------------------------------- Static funcionales no saben que pertenecen a la clase
     @staticmethod
     def probar_conexion() -> None:
         try:
@@ -25,6 +31,37 @@ class DBManager:
         except sqlite3.Error as e:
             print(f"❌ Error al conectar con SQLite: {e}")
 
+    # ----------------------------------------- Static de clase, pueden usar cls para referenciar a la clase y sus propiedades
+    @classmethod
+    def insertar_registros(cls) -> None:
+        try:
+            with closing(sqlite3.connect(DB_PATH)) as conn:
+                with conn:
+                    cursor = conn.cursor()
+
+                    cursor.execute(cls._query_inster)
+
+                    cursor.close()
+
+            print(f"✅ Registro insertado correctamente")
+        except sqlite3.Error as e:
+            print(f"❌ Error de base de datos: {e}")
+
+    @classmethod
+    def borrar_db(cls) -> None:
+        try:
+            with closing(sqlite3.connect(DB_PATH)) as conn:
+                with conn:
+                    cursor = conn.cursor()
+
+                    cursor.execute(cls._query_drop)
+
+                    cursor.close()
+
+            print(f"✅ Base de datos borrada exitosamente")
+        except sqlite3.Error as e:
+            print(f"❌ Error de base de datos: {e}")
+
     @classmethod
     def crear_db_open_close(cls) -> None:
         try:
@@ -34,12 +71,15 @@ class DBManager:
 
             cursor.execute(cls._query_create)
 
+            # Confirmar cambios | conn.rollback() para revertirlos, con width se hace solo
+            conn.commit()
+
             cursor.close()
             conn.close()
 
             print("✅ Tabla 'productos' verificada/creada con éxito.")
         except sqlite3.Error as e:
-            print(f"❌ Error al conectar con SQLite: {e}")
+            print(f"❌ Error de base de datos: {e}")
 
     @classmethod
     def crear_db_closing_with(cls) -> None:
@@ -58,4 +98,4 @@ class DBManager:
                     
             print("✅ Tabla 'productos' verificada/creada con éxito.")
         except sqlite3.Error as e:
-            print(f"❌ Error al conectar con SQLite: {e}")
+            print(f"❌ Error de base de datos: {e}")
--- curso-python/senior/databases/main.py
+++ curso-python/senior/databases/main.py
@@ -1,4 +1,6 @@
 from logic.db_manager import DBManager
 
 if __name__ == "__main__":
+    DBManager.borrar_db()
     DBManager.crear_db_closing_with()
+    DBManager.insertar_registros()
