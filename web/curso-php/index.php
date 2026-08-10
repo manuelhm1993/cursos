eddb2e4fedf615a81615f91e003d5810b353d226
Manuel Henriquez <manuelhm1993@gmail.com>
2026-08-10 17:27:31 -0400

    feature: novedades de php 8 el uso de la palabra reservada const para constantes sobre el define legacy y la expresión match que viene a ser una mejora del switch case y el look up table

--- web/curso-php/index.php
+++ web/curso-php/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Primer sitio web</title>
+    <title>PHP 8.3.x - Constantes</title>
 </head>
 <body>
     <?php echo '<h1>Sistemas en línea. PHP 8.3 Operativo en WSL2.</h1>'; ?>
