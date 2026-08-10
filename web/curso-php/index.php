e9306b1ced2b87f7f826445ee92625709024454a
Manuel Henriquez <manuelhm1993@gmail.com>
2026-08-10 15:52:29 -0400

    feature: infraestructura para proyectos php8.3 con un servidor apache ligero en docker aislado en el puerto 8081 y orquestado con su yml

---
+++ web/curso-php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primer sitio web</title>
+</head>
+<body>
+    <?php echo '<h1>Sistemas en línea. PHP 8.3 Operativo en WSL2.</h1>'; ?>
+</body>
+</html>
