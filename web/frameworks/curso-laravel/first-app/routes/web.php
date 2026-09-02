fe496977a5f4f398d2ce13030496eabd9aa10079
Manuel Henriquez <manuelhm1993@gmail.com>
2026-09-02 07:32:43 -0400

    feature: uso de rutas, rutas parametrizadas y parámetros opcionales

--- web/frameworks/curso-laravel/first-app/routes/web.php
+++ web/frameworks/curso-laravel/first-app/routes/web.php
@@ -2,6 +2,59 @@
 
 use Illuminate\Support\Facades\Route;
 
+// HOME
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('categories', function () {
+    $categories = [
+        'Fideos',
+        'Tomates',
+        'Arroz'
+    ];
+
+    foreach ($categories as $category) {
+        echo "{$category} <br>";
+    }
+});
+
+Route::get('categories/{name}', function(string $name) {
+    return "Productos de $name";
+});
+
+Route::get('products/{category?}', function(?string $category = null) {
+    $categories = [
+        "Fideos" => [
+            "Moñitos",
+            "Fideos largos",
+            "Cabello de ángel",
+        ],
+        "Verduras" => [
+            "Tomates",
+            "Lechuga",
+            "Cebolla",
+        ],
+    ];
+
+    // Si la categoría no fue enviada, se muestran todos los productos
+    if (is_null($category)) {
+        foreach ($categories as $key => $value) {
+            echo "Categoría $key <br>";
+            foreach($value as $product) {
+                echo "Producto: $product <br>";
+            }
+        }
+        return;
+    }
+
+    // Si la categoría existe se muestran sus productos
+    if (array_key_exists($category, $categories)) {
+        foreach ($categories[$category] as $product) {
+            echo "Producto: $product <br>";
+        }
+        return;
+    }
+
+    echo "Categoría no encontrada";
 });
