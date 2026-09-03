e98c1527b9dbe4c25e4713810bacff3981e0ed64
Manuel Henriquez <manuelhm1993@gmail.com>
2026-09-02 22:02:40 -0400

    feature: limpieza total y desacomplamiento de las rutas a la lógica del negocio y delegar la responsabilidad a los controladores

--- web/frameworks/curso-laravel/first-app/routes/web.php
+++ web/frameworks/curso-laravel/first-app/routes/web.php
@@ -5,9 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
-use App\Utilities\Common;
-
-$categories = Common::getCategories();
+use App\Http\Controllers\ProductController;
 
 // HOME - USO DE CONTROLADOR PARA DELEGAR LA LÓGICA DEL NEGOCIO
 Route::get('/', [HomeController::class, 'index']);
@@ -18,39 +16,7 @@ Route::prefix('categories')->name('categories.')->controller(CategoryController:
     Route::get('/{name}', 'show')->name('show');
 });
 
-// PRODUCTOS
-Route::prefix('products')->name('products.')->group(function () use ($categories) {
-    Route::get('/{category?}', function(?string $category = null) use ($categories) {
-        // Si la categoría no fue enviada, se muestran todos los productos
-        if (is_null($category)) {
-            /*$products = [];
-            foreach ($categories as $key => $value) {
-                foreach($value as $product) {
-                    $products[] = $product;
-                }
-            }*/
-
-            /*$products = [];
-            foreach ($categories as $value) {
-                $products = array_merge($products, $value);
-            }*/
-
-            /*$products = array_reduce($categories, function($carry, $item) {
-                return array_merge($carry, $item);
-            }, []);*/
-
-            $products = array_merge(...array_values($categories));
-
-            return view('products.index', compact('products'));
-        }
-
-        // Si la categoría existe se muestran sus productos
-        if (array_key_exists($category, $categories)) {
-            $products = $categories[$category];
-
-            return view('products.index', compact('products'));
-        }
-
-        echo "Categoría no encontrada";
-    })->name('show');
+// PRODUCTOS - USO DE CONTROLADOR PARA DELEGAR LA LÓGICA DEL NEGOCIO
+Route::prefix('products')->name('products.')->controller(ProductController::class)->group(function() {
+    Route::get('/{category?}', 'show')->name('show');
 });
