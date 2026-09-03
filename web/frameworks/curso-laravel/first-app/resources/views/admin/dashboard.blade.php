b6219f2c205824828f741d522c8f1f57d3693b54
Manuel Henriquez <manuelhm1993@gmail.com>
2026-09-03 04:51:19 -0400

    feature: implementación manual de bakstrap y descarga de las dependencias manualmente y uso de cdn para la personalización para el proyecto

---
+++ web/frameworks/curso-laravel/first-app/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.admin')
+
+<h1>HOME ADMIN</h1>
---
+++ web/frameworks/curso-laravel/first-app/resources/views/admin/includes/dashboard-aside.blade.php
@@ -0,0 +1,25 @@
+<ul class="nav">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('admin.dashboard') }}">
+            <i class="nav-icon la la-lg la-dashboard"></i> Dashboard
+        </a>
+    </li>
+    <li class="nav-title">Administrador</li>
+    <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#">
+            <i class="nav-icon la la-lg la-bank"></i> Categorías
+        </a>
+        <ul class="nav-dropdown-items">
+            <li class="nav-item">
+                <a class="nav-link" href="base/breadcrumb.html">
+                    <i class="nav-icon la la-lg la-puzzle"></i> Crear
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="base/cards.html">
+                    <i class="nav-icon la la-lg la-puzzle"></i> Editar
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
