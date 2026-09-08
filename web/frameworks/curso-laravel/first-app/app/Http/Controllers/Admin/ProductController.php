a144c2f1e96d53505ffa9d7a533a00edaf5b11c6
Manuel Henriquez <manuelhm1993@gmail.com>
2026-09-08 06:58:34 -0400

    feature: soft delete, se usa para cuando no queremos borrar en cascada y tampoco queremos setear en null, entonces se hace un borrado lógico que es por detrás un update de la fecha de borrado, si se desea obtener los registros borrados se usa un widthTrashed en la relación de los modelos y el softdelete se usa en el modelo a borrar como campod e base de datos

--- web/frameworks/curso-laravel/first-app/app/Http/Controllers/Admin/ProductController.php
+++ web/frameworks/curso-laravel/first-app/app/Http/Controllers/Admin/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::with('category')->get();
 
         return view('admin.products.index', compact('products'));
     }
