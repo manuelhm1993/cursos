d168be25662d4420f7fa1f629fe38be55cfdac5c
Manuel Henriquez <manuelhm1993@gmail.com>
2026-09-10 05:44:01 -0400

    feature: uso de SChedule command para ejecutar jobs desde laravel

--- web/frameworks/curso-laravel/first-app/routes/console.php
+++ web/frameworks/curso-laravel/first-app/routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Elimina mensualmente las compras que tengan más de 30 días
+Schedule::command('mh:eliminar-compra')->daily();
