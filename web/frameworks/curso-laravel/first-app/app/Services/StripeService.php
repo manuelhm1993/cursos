a4ba0b960fec20ee1d9cf71621e7644aebbc7b98
Manuel Henriquez <manuelhm1993@gmail.com>
2026-09-11 12:36:30 -0400

    feat: se creó una exepción personalizada para jerarquía de errores, se repazó que un constructor no implementa override, no lleva return y que la excepción se lanza en la creación de la intención. También se corrigió el bloqueo de caché con el parámetro de expiración

--- web/frameworks/curso-laravel/first-app/app/Services/StripeService.php
+++ web/frameworks/curso-laravel/first-app/app/Services/StripeService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Exceptions\StripeException;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
 
@@ -18,16 +19,19 @@ class StripeService
 
     public function crearIntencionDePago(float $total, int $compraId): PaymentIntent 
     {
-        // throw new \Exception("Stripe no funciona");
-
-        return PaymentIntent::create([
-            // Multiplicamos por 100 y redondeamos para evitar pérdida de precisión en decimales
-            'amount' => (int) round($total * 100), /* La banca prefiere cobros en centavos para evitar el punto flotante */
-            'currency' => 'usd',
-            'metadata' => [
-                'compra_id' => $compraId,
-            ],
-        ]);
+        try {
+            return PaymentIntent::create([
+                // Multiplicamos por 100 y redondeamos para evitar pérdida de precisión en decimales
+                'amount' => (int) round($total * 100), /* La banca prefiere cobros en centavos para evitar el punto flotante */
+                'currency' => 'usd',
+                'metadata' => [
+                    'compra_id' => $compraId,
+                ],
+            ]);
+        } 
+        catch(\Exception $e) {
+            throw new StripeException;
+        }
     }
 
     public function obtenerIntencionDePago(): void 
